5d9d10c9e998c6e1f059f563fab5eb871190a675
fraanfx <fran.am95@gmail.com>
2023-04-15 19:44:27 +0200

    first-commit

---
+++ client/src/components/common/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { ProfileProps } from 'interfaces/common';
+
+const Profile = ({ type, name, avatar, email, properties } : ProfileProps) => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
