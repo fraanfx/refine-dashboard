8f81ccc369c8e7e3a38ad0ba506746b8fd2754e5
fraanfx <fran.am95@gmail.com>
2023-04-21 17:34:45 +0200

    Finished first mvp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -137,8 +137,8 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
-         // dataProvider={dataProvider("https://refine-dashboard-8sgj.onrender.com/api/v1")}
+          //dataProvider={dataProvider("http://localhost:8080/api/v1")}
+         dataProvider={dataProvider("https://refine-dashboard-8sgj.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
